581dd9730b9b8ee1b5250b24eeb35cc56c3cb6d2
MasterTVC <googlythattuyet@gmail.com>
2019-03-09 14:01:06 +0900

    update

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// mongodb://localhost:27017/firstapp   - mongodb://vanxcuong:12345612@ds117759.mlab.com:17759/firstapp
+mongoose.connect('mongodb://vanxcuong:abc123@ds123371.mlab.com:23371/vietvan', { useNewUrlParser: true } ,function(err){
+  if(err){
+    console.log("Connect database failed !!!");
+  }else{
+    console.log("Connect database success !!!");
+  }
+});
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/javascripts/actions.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function () {
+    let pwtouch = document.getElementById("pwtouch") // Khung chọn mật khẩu
+    pwtouch.addEventListener("click",function(e){
+        console.log(e);
+    })
+})
